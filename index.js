84bf91de82d84773ee26212ead7bbcd7258be780
YadinaM <yadina.moreira@gmail.com>
2023-10-13 12:36:05 +0200

    feat: add cors

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const cors = require("cors");
+app.use(cors());
+
 app.get('/api/v1/messages', (req, res) => {
   res.json({
     status : "success",
     message: "GET messages", 
-    data: [{git 
+    data: [{
       //als data een paar messages verzinnen,
     }]
   })
